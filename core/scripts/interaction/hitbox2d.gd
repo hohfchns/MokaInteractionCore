be4b6c686076fdc40a737ab8bb3db6c0231ed1ca
Sahar Shulman <hohfchns@gmail.com>
2025-05-31 12:57:29 +0300

    Fix to ignore groups

--- core/scripts/interaction/hitbox2d.gd
+++ core/scripts/interaction/hitbox2d.gd
@@ -13,13 +13,16 @@ func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 
 func _on_area_entered(area: Area2D) -> void:
-	if area is Hurtbox2D:
-		for group in ignore_groups:
-			if area.is_in_group(group):
-				return
-		
-		if ( area.trigger(hit_data, self) ):
-			hurtbox_detected.emit(area)
+	if area is not Hurtbox2D:
+		return
+	
+	for group in ignore_groups:
+		if area.is_in_group(group) \
+		or group in area.ignore_groups:
+			return
+	
+	if ( area.trigger(hit_data, self) ):
+		hurtbox_detected.emit(area)
 
 func disable() -> void:
 	set_deferred("monitoring", false)
@@ -31,4 +34,3 @@ func enable() -> void:
 
 func disabled() -> bool:
 	return monitoring and monitorable
-
--- core/scripts/interaction/hitbox3d.gd
+++ core/scripts/interaction/hitbox3d.gd
@@ -13,13 +13,16 @@ func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 
 func _on_area_entered(area: Area3D) -> void:
-	if area is Hurtbox3D:
-		for group in ignore_groups:
-			if area.is_in_group(group):
-				return
-		
-		if ( area.trigger(hit_data, self) ):
-			hurtbox_detected.emit(area)
+	if area is not Hurtbox3D:
+		return
+	
+	for group in ignore_groups:
+		if area.is_in_group(group) \
+		or group in area.ignore_groups:
+			return
+	
+	if ( area.trigger(hit_data, self) ):
+		hurtbox_detected.emit(area)
 
 func disable() -> void:
 	set_deferred("monitoring", false)
